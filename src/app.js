4631bfce7b627c5ca7b32e56e48b287da598b875
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-30 22:38:53 -0300

    Agregar elementos visuales

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + "/public"));
+
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
